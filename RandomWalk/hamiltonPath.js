b63b7206031fdb16191cf27d453516f6454e4332
yzhang33 <youmingzhang19@gmail.com>
2021-02-08 22:54:25 -0500

    saw

--- RandomWalk/hamiltonPath.js
+++ RandomWalk/hamiltonPath.js
@@ -3,11 +3,10 @@ function HamiltonPath(){
      * generate hamiltonian ath start with trival this.path that traverse from top conrner to the bottom right
      */
 
-    this.this.path = new Array(4*4);
+    this.path = new Array(4*4);
 
-    this.generateHamiltonianthis.Path=function(n,q){
+    this.generateHamiltonianPath=function(n,q){
         var i,j;
-
         for(i=0;i<n;i++){
             for(j=0;j<n;j++){
                 if(i%2 == 0){
@@ -22,20 +21,113 @@ function HamiltonPath(){
 
             }
         }
-
         nsuccess = 0;
         nattempts = 0;
-        //moves needed for backbite algorithm
+        //moves needed for backbite algorithm (this math I stolen from google)
         nmoves = q*10.0 * n * n * Math.pow(Math.log(2.+n),2);
         while(nsuccess < nmoves)
         {
-            success = backbite(n,this.path);
+            success = this.backbite(n);
             if (success) nsuccess++;
             nattempts++;
         }
+        //randomly modify laast attempts
+        for (i=0; i<nattempts; i++)
+        {
+            success = this.backbite(n);
+        }
+
     };
 
-    this.backbite = function(n,path){
+    this.backbite = function(n){
+        var x,y;
+        var xedge,yedge;
+        var iedge, addEdge;
+        var i,j;
+        var success;
+        //start from (0,0) or (n-1,n-1) with 1/2 probablity
+        var prob=Math.floor(Math.random()*2);
+        if(prob == 0){
+            //start (0,0)
+            x = this.path[0][0];
+            y = this.path[0][1];
+            //check current start position 
+            xedge = ((x == 0) || (x == n-1));
+            yedge = ((y == 0) || (y == n-1));
+            //at corner
+            if(xedge && yedge){
+                // 1/3 acceptance probablity at the corner
+                addEdge = Math.floor(Math.random()*3) - 2;
+            }else if(xedge || yedge){
+                // 2/3 acceptance probablity at edges
+                addEdge =  Math.floor(Math.random()*3) - 1;
+            }else{
+                // can accept all 3 edges
+                addEdge =  Math.floor(Math.random()*3);
+            }
+            success = (addEdge >= 0);
+            iedge = 0;
+            i = 3;
+            while(iedge <= addEdge){
+                dx = Math.abs(x - this.path[i][0]);
+                dy = Math.abs(y - this.path[i][1]);
+                //neighbor points
+                if(dx+dy == 1){
+                    //found empty edge to add 
+                    if(iedge == addEdge){
+                        //reverse check cycle from 0 to i -1;
+                        var len = (i-1) / 2;
+                        for(j=0;j<len;j++){
+                            var temp = this.path[j];
+                            this.path[j] = this.path[i-1-j];
+                            this.path[i-1-j] = temp; 
+                        }
+                    }
+                    iedge++;
+                }
+                i += Math.max(2,dx+dy-1);
+            }
+        }else{
+            var endPoint = n*n - 1;
+            x = this.path[endPoint][0];
+            y = this.path[endPoint][1];
+            xedge = ((x == 0) || (x == n-1));
+            yedge = ((y == 0) || (y == n-1));
+            //at corner
+            if(xedge && yedge){
+                // 1/3 acceptance probablity at the corner
+                addEdge = Math.floor(Math.random()*3) - 2;
+            }else if(xedge || yedge){
+                // 2/3 acceptance probablity at edges
+                addEdge =  Math.floor(Math.random()*3) - 1;
+            }else{
+                // can accept all 3 edges
+                addEdge =  Math.floor(Math.random()*3);
+            }
+            success = (addEdge >= 0);
+            iedge = 0;
+            i = endPoint - 3;
+            while(iedge <= addEdge){
+                dx = Math.abs(x - this.path[i][0]);
+                dy = Math.abs(y - this.path[i][1]);
+                //neighbor points
+                if(dx+dy == 1){
+                    //found empty edge to add 
+                    if(iedge == addEdge){
+                        //reverse walk from end point to 0;
+                        var len = (endPoint-i-1) / 2;
+                        for(j=0;j<len;j++){
+                            var temp = this.path[endPoint-j];
+                            this.path[endPoint-j] = this.path[i+1+j];
+                            this.path[i+1+j] = temp; 
+                        }
+                    }
+                    iedge++;
+                }
+                i -= Math.max(2,dx+dy-1);
+            }
+        }
+        return success;
 
     }
 
--- RandomWalk/sketch.js
+++ RandomWalk/sketch.js
@@ -1,20 +1,22 @@
 var col, row;
 var w =150;
 var grid=[];
-path = new Array(4*4);
-pre = [];
+var path;
+var pre = [];
+var hamiltonianPath;
 
 function setup(){
     createCanvas(600,600);
     background(51);
     row = floor(width/w);
     col = floor(height/w);
-    //draw grids
+
     for(var i=0;i<row;i++){
         for(var j=0;j<col;j++){
             grid.push(new Cell(i,j));
         }
     }
+    hamiltonianPath = new HamiltonPath();
 
 }
 
@@ -23,22 +25,23 @@ function draw(){
     for(var cell of grid){
         cell.show();
     }
-    //generate hamiltonian path and draw it
     drawPath();
     noLoop();
 }
-
 function drawPath(){
-    generatePath(4);
-    //console.log(path);
+    hamiltonianPath.generateHamiltonianPath(4,1);
+    path = hamiltonianPath.path;
     for(var i=0;i<path.length;i++){
-        var x = path[i][0]*w+75;
-        var y = path[i][1]*w+75;
-        strokeWeight(10);
-        stroke(0);
-        line(pre[0],pre[1],x,y);
-        pre = [x,y];
-        //point(x,y);
+                var x = path[i][0]*w+75;
+                var y = path[i][1]*w+75;
+                strokeWeight(10);
+                stroke(0);
+                if(pre[0] && pre[1]){
+                    line(pre[0],pre[1],x,y);
+                }
+                //line(pre[0],pre[1],x,y);
+                pre = [x,y];
+                //point(x,y);
     }
     //draw start(purple) and finish(yellow) point
     var i = path[0][0]*w+75;
@@ -49,28 +52,6 @@ function drawPath(){
     j=path[path.length-1][1]*w+75;
     stroke(255,255,0);
     point(i,j);
-    
-}
-
-
-function generatePath(n){
-    var i,j;
-    //path start from 0,0 extend to the end.traverse from top to 
-    //bottom for even number of grids and bottom to top for odd grids
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(i%2 == 0){
-                for(j=0;j<n;j++){
-                    path[i*n+j]=[i,j];
-                }
-            }else{
-                for(j=0;j<n;j++){
-                    path[i*n+j]=[i,n-j-1];
-                } 
-            }
-
-        }
-    }
 }
 
 function Cell(i,j){
